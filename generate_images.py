aee7b805132b814c7d8fea8dfa9e1fae83f63809
Ubuntu <ubuntu@ip-172-31-42-121.us-east-2.compute.internal>
2022-11-17 21:10:47 +0000

    added images output dir to generate_images script

--- generate_images.py
+++ generate_images.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from diffusers import StableDiffusionPipeline
 
@@ -10,12 +11,17 @@ pipe = pipe.to(device)
 
 # Create a list of images and return the list of saved filenames 
 # in the same order as the utterances in the transcript
-def text_to_images(utterances):
+def text_to_images(utterances, request_dir):
+  os.makedirs(request_dir, exist_ok=True)
   filenames = []
   for k, utt in enumerate(utterances):
     image = pipe(utt['transcript'], num_inference_steps=50).images[0]
 
-    filenames.append(f'{k}.jpg')
+    filenames.append(f'{request_dir}/{k}.jpg')
     image.save(filenames[-1])
 
   return filenames
+
+
+utts =[{'transcript': 'hello'}, {'transcript': 'chasing after car'}]
+text_to_images(utterances=utts, request_dir='hellohere')
